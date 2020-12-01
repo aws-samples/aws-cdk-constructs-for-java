3710ea2320bd585d4eac05b466c81d06c419bc8e
Tim Mattison <tim@mattison.org>
2020-12-01 10:06:02 -0500

    Added another helper function

--- src/main/java/com/aws/samples/cdk/constructs/iam/permissions/iot/IotResources.java
+++ src/main/java/com/aws/samples/cdk/constructs/iam/permissions/iot/IotResources.java
@@ -1,9 +1,6 @@
 package com.aws.samples.cdk.constructs.iam.permissions.iot;
 
-import com.aws.samples.cdk.constructs.iam.permissions.iot.resources.Topic;
-import com.aws.samples.cdk.constructs.iam.permissions.iot.resources.TopicFilter;
-import com.aws.samples.cdk.constructs.iam.permissions.iot.resources.ImmutableTopic;
-import com.aws.samples.cdk.constructs.iam.permissions.iot.resources.ImmutableTopicFilter;
+import com.aws.samples.cdk.constructs.iam.permissions.iot.resources.*;
 
 public class IotResources {
     public static Topic topic(String topic) {
@@ -12,5 +9,9 @@ public class IotResources {
 
     public static TopicFilter topicFilter(String topicFilter) {
         return ImmutableTopicFilter.builder().topicFilter(topicFilter).build();
+    }
+
+    public static ClientId clientId(String clientId) {
+        return ImmutableClientId.builder().clientId(clientId).build();
     }
 }
