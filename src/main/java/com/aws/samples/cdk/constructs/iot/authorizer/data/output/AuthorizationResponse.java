661c8098886ade8c58160ade16b17b31daa5c6ba
Tim Mattison <tim@mattison.org>
2021-02-03 15:25:14 -0500

    More list changes to fix serialization issues

--- src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/output/AuthorizationResponse.java
+++ src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/output/AuthorizationResponse.java
@@ -1,11 +1,14 @@
 package com.aws.samples.cdk.constructs.iot.authorizer.data.output;
 
-import io.vavr.collection.List;
+// NOTE: Do not change this to io.vavr.collection.List! See comment below.
+
+import java.util.List;
 
 public class AuthorizationResponse {
     public boolean isAuthenticated;
     public String principalId;
     public int disconnectAfterInSeconds;
     public int refreshAfterInSeconds;
+    // This must be a java.util.List, not an io.vavr.collection.List, so serialization to JSON in Lambda works
     public List<PolicyDocument> policyDocuments;
 }
--- src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/output/PolicyDocument.java
+++ src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/output/PolicyDocument.java
@@ -1,8 +1,11 @@
 package com.aws.samples.cdk.constructs.iot.authorizer.data.output;
 
-import io.vavr.collection.List;
+// NOTE: Do not change this to io.vavr.collection.List! See comment below.
+
+import java.util.List;
 
 public class PolicyDocument {
     public String Version = "2012-10-17";
+    // This must be a java.util.List, not an io.vavr.collection.List, so serialization to JSON in Lambda works
     public List<Statement> Statement;
 }
