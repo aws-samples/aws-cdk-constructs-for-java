5e677a2db87ccd0402cb1e715b30d216d5bdc49c
Tim Mattison <tim@mattison.org>
2021-02-03 15:03:33 -0500

    Changed protocol list back to a normal Java list so Lambda serialization works

--- src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/input/AuthorizationRequest.java
+++ src/main/java/com/aws/samples/cdk/constructs/iot/authorizer/data/input/AuthorizationRequest.java
@@ -1,10 +1,13 @@
 package com.aws.samples.cdk.constructs.iot.authorizer.data.input;
 
-import io.vavr.collection.List;
+// NOTE: Do not change this to io.vavr.collection.List! See comment below.
+
+import java.util.List;
 
 public class AuthorizationRequest {
     public String token;
     public boolean signatureVerified;
+    // This must be a java.util.List, not an io.vavr.collection.List, so de-serialization from JSON in Lambda works
     public List<String> protocols;
     public ProtocolData protocolData;
     public ConnectionMetadata connectionMetadata;
