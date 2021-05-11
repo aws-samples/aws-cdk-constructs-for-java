aa24de8778855928a65bda16da38346f264bda61
Tim Mattison <tim@mattison.org>
2021-05-11 09:57:18 -0400

    Updated to CDK 1.103.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ group = "local"
 version = "1.0-SNAPSHOT"
 
 // Specify all of our dependency versions
-val awsCdkVersion = "1.96.0"
+val awsCdkVersion = "1.103.0"
 val awsSdkVersion = "2.16.33"
 val vavrVersion = "0.10.3"
 val commonsLangVersion = "3.12.0"
