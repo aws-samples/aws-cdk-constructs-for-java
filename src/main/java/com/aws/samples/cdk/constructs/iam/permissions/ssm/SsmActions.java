f781e76668045a3ef0dd3cb86f7b4df4a2a5aa47
Tim Mattison <tim@mattison.org>
2021-06-30 19:21:26 -0400

    Added describe activations constant

--- src/main/java/com/aws/samples/cdk/constructs/iam/permissions/ssm/SsmActions.java
+++ src/main/java/com/aws/samples/cdk/constructs/iam/permissions/ssm/SsmActions.java
@@ -1,8 +1,11 @@
 package com.aws.samples.cdk.constructs.iam.permissions.ssm;
 
+import com.aws.samples.cdk.constructs.iam.permissions.ssm.actions.DescribeActivations;
 import com.aws.samples.cdk.constructs.iam.permissions.ssm.actions.DescribeInstanceInformation;
+import com.aws.samples.cdk.constructs.iam.permissions.ssm.actions.ImmutableDescribeActivations;
 import com.aws.samples.cdk.constructs.iam.permissions.ssm.actions.ImmutableDescribeInstanceInformation;
 
 public class SsmActions {
     public static DescribeInstanceInformation describeInstanceInformation = ImmutableDescribeInstanceInformation.builder().build();
+    public static DescribeActivations describeActivations = ImmutableDescribeActivations.builder().build();
 }
