fe8b435cde977dab53cdbc22858a4482ec1e3912
Tim Mattison <tim@mattison.org>
2021-08-20 13:33:06 -0400

    Fixed Function resource to be a LambdaResource instead of an IotDataPlaneResource

--- src/main/java/com/aws/samples/cdk/constructs/iam/permissions/lambda/resources/Function.java
+++ src/main/java/com/aws/samples/cdk/constructs/iam/permissions/lambda/resources/Function.java
@@ -1,12 +1,11 @@
 package com.aws.samples.cdk.constructs.iam.permissions.lambda.resources;
 
-import com.aws.samples.cdk.constructs.iam.permissions.iot.dataplane.resources.IotDataPlaneResource;
 import org.immutables.gson.Gson;
 import org.immutables.value.Value;
 
 @Gson.TypeAdapters
 @Value.Immutable
-public abstract class Function implements IotDataPlaneResource {
+public abstract class Function implements LambdaResource {
     public abstract String getFunctionName();
 
     @Override
