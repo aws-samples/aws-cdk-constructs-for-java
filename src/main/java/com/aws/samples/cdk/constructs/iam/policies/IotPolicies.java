efb5d2af8b9d4fecfd4cfbb409ef0ceecb4bfecd
Tim Mattison <tim@mattison.org>
2021-08-31 22:16:40 -0400

    Added AWS IoT config read only access managed policy constant

--- src/main/java/com/aws/samples/cdk/constructs/iam/policies/IotPolicies.java
+++ src/main/java/com/aws/samples/cdk/constructs/iam/policies/IotPolicies.java
@@ -9,8 +9,13 @@ import static com.aws.samples.cdk.constructs.iam.permissions.SharedPermissions.I
 
 public class IotPolicies {
     public static final ServicePrincipal IOT_SERVICE_PRINCIPAL = new ServicePrincipal("iot");
+
     private static final String AWS_IOT_DATA_ACCESS_POLICY_NAME = "AWSIoTDataAccess";
     public static final IManagedPolicy AWS_IOT_DATA_ACCESS_POLICY = ManagedPolicy.fromAwsManagedPolicyName(AWS_IOT_DATA_ACCESS_POLICY_NAME);
+
+    private static final String AWS_IOT_CONFIG_READ_ONLY_ACCESS_POLICY_NAME = "AWSIoTConfigReadOnlyAccess";
+    public static final IManagedPolicy AWS_IOT_CONFIG_READ_ONLY_ACCESS_POLICY = ManagedPolicy.fromAwsManagedPolicyName(AWS_IOT_CONFIG_READ_ONLY_ACCESS_POLICY_NAME);
+
     private static final PolicyStatementProps SEARCH_INDEX_STATEMENT_PROPS = PolicyStatementProps.builder()
             .effect(Effect.ALLOW)
             .resources(Collections.singletonList(ALL_RESOURCES))
